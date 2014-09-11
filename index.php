ede09edebae4cd546a6f3335eaa6e095c97f80b2
Guillaume Sylvestre <gsylvestre@gmail.com>
2014-09-11 14:19:37 +0000

    Change autoload for linux
    git-svn-id: file:///var/svn/skp/trunk@6 54603a6e-7c00-409b-81b0-d312d09a75cd

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
     //autoloading classes
-    spl_autoload_register();
+    spl_autoload_register(function($c){@include preg_replace('#\\\|_(?!.+\\\)#','/',$c).'.php';});
     require_once("vendor/autoload.php");
 
     //go
-    $app = new Controller\App();
+    $app = new \Controller\App();
     $app->run();
