7a1fd50752ba4c2105293b0ffed037f72e0350f6
Dario Spagnolo <i@dariospagnolo.org>
2014-09-27 18:11:53 +0000

    Missing file
    git-svn-id: file:///var/svn/skp/trunk@131 54603a6e-7c00-409b-81b0-d312d09a75cd

---
+++ l10n/JSTranslations.php
@@ -0,0 +1,21 @@
+<?php
+
+    namespace l10n;
+
+    class JSTranslations {
+
+        function getJSTranslations() {
+
+            return array(
+                "or"    => _("or"),
+                
+                "panel" => array(
+                    "haveToBeSigned"    => _("You have to be signed in to do that !"),
+                    "signIn"            => _("Sign in !"),
+                    "createAccount"     => _("Create an account !")
+                )
+            );
+       }
+
+    }
+
