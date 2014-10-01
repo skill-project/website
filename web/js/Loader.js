d75944395f3ce07540d66b18e0b287d79d473ca1
Dario Spagnolo <i@dariospagnolo.org>
2014-10-01 16:56:24 +0000

    Loader files missing
    git-svn-id: file:///var/svn/skp/trunk@151 54603a6e-7c00-409b-81b0-d312d09a75cd

---
+++ web/js/Loader.js
@@ -0,0 +1,41 @@
+var Loader = function() {
+	this.isVisible = false;
+	this.isWaitingToShow = false;
+	this.timer;
+	this.cl;
+}
+
+Loader.prototype.show = function() {
+	if (typeof this.cl == "undefined") {
+		this.timer = setTimeout(function() {
+			loader.cl = new CanvasLoader('loader');
+			loader.cl.setColor('#fef7f9');
+			loader.cl.setShape('spiral');
+			loader.cl.setDiameter(47);
+			loader.cl.setDensity(22);
+			loader.cl.setRange(1.2);
+			loader.cl.setSpeed(1);
+			loader.cl.setFPS(30);
+			loader.cl.show();
+			
+			this.isVisible = true;
+			this.isWaitingToShow = false;
+		}, 200);
+		this.isWaitingToShow = true;
+	}
+}
+
+Loader.prototype.hide = function() {
+	if (this.isWaitingToShow == true) {
+		clearTimeout(this.timer);
+		this.isVisible = false;
+	}	
+	if (typeof this.cl != "undefined") {
+		this.cl.kill();
+
+		this.isVisible = true;
+		this.isWaitingToShow = false;
+
+		delete this.cl;
+	}
+}
