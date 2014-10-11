9975678a23e59729e0144886cb0acea992e2d76a
Guillaume Sylvestre <gsylvestre@gmail.com>
2014-10-11 14:38:09 +0000

    home real content
    git-svn-id: file:///var/svn/skp/trunk@241 54603a6e-7c00-409b-81b0-d312d09a75cd

--- View/pages/home.php
+++ View/pages/home.php
@@ -1,10 +1,10 @@
 <div id="home-top-panel">
 	<div class="container">
 		<div class="left-content">
-			<h1>Show the world<br />what the world can do.</h1>
-			<p id="tagline">Explore all the human skills. Map them out.</p>
+			<h1>We are mapping every human skill out there.<br />And we need your help.</h1>
+			<p id="tagline">How many skills do you have? Think about them, and map them out.</p>
 
-			<div id="home-btn-go" class="home-btn"><a href="<?= \Controller\Router::url("graph"); ?>">Start skillwalking</a></div>
+			<div id="home-btn-go" class="home-btn"><a href="<?= \Controller\Router::url("graph"); ?>">Enter Skill Project</a></div>
 			<div id="home-btn-learn" class="home-btn"><a href="<?= \Controller\Router::url("project"); ?>">Learn more</a></div>
 		</div>
 
@@ -15,22 +15,23 @@
 <section id="home-main-panel">
 	<div class="container">
 		<img class="home-illus brain-illus" alt="" src="img/SKP-page-home-brain.png" />
-		<h2>Do you know everything you know ?</h2>
-		<p>sda fds fs fs fdfas eq wrew rew rewqr r ewr ew e </p>
+		<h2>Welcome to Skill Project! What can you do?</h2>
+		<p>At Skill Project, we believe everyone has a unique share of humanity skills.<br />Become part of our skill loving community and share what you can do!</p>
 
 		<img class="home-illus network-illus" alt="" src="img/SKP-page-home-network.png" />
-		<h2>Do you know everything you know ?</h2>
-		<p>sda fds fs fs fdfas eq wrew rew rewqr r ewr ew e </p>
+		<h2>Every skill is connected to another one</h2>
+		<p>So you know how to program? Well, this is definitely a skill! And “C++ programming” or “PHP programming” are simply sub-skills of “Programming”. And if you also know Wordpress or Symfony, they are a sub-skills of “PHP”.
+			<br />You get the picture?</p>
 
 		<img class="home-illus circle-illus" alt="" src="img/SKP-page-home-circle.png" />
-		<h2>Do you know everything you know ?</h2>
-		<p>sda fds fs fs fdfas eq wrew rew rewqr r ewr ew e </p>
+		<h2>Spread the word</h2>
+		<p>By joining Skill Project you are contributing your unique understanding of your skills. The only way we can meet our goal is to create a large and diverse community of people who are willing to do the same.<br />Go tell your friends!</p>
 	</div> 
 </section>
 <section id="home-bottom-panel" class="green">
 	<div class="container">
-		<h2>Ready to go ?</h2>
-		<p>Starts now skillwalking amongst the skill and create your own !</p>
-		<div id="home-btn-go-bottom" class="home-btn"><a href="<?= \Controller\Router::url("graph"); ?>">Start skillwalking</a></div>
+		<h2>Ready to go?</h2>
+		<p>Start exploring the skills and enjoy adding your own!</p>
+		<div id="home-btn-go-bottom" class="home-btn"><a href="<?= \Controller\Router::url("graph"); ?>">Enter Skill Project</a></div>
 	</div> 
 </section>
