b6fc9a56d0a5bc15a06b21ce4f108bf9d1c2cb22
Guillaume Sylvestre <gsylvestre@gmail.com>
2014-10-11 13:13:47 +0000

    add bio for raphael
    git-svn-id: file:///var/svn/skp/trunk@238 54603a6e-7c00-409b-81b0-d312d09a75cd

--- View/pages/project.php
+++ View/pages/project.php
@@ -217,7 +217,7 @@
         <div class="project-col">
             <div id="raphael-pic" class="team-pic"></div>
             <h4>RAPHAEL BOUSQUET</h4>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+            <p>Raphael loves to speak latin like that: Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
         </div>
     </div>
 </section>
