301973bfa1d9b6db0a73aa6bdfd8fa48de828fbf
Dario Spagnolo <i@dariospagnolo.org>
2014-10-11 15:32:53 +0000

    Better discussion style
    git-svn-id: file:///var/svn/skp/trunk@245 54603a6e-7c00-409b-81b0-d312d09a75cd

--- View/panels/discussion-messages.php
+++ View/panels/discussion-messages.php
@@ -1,12 +1,18 @@
 
     <?php foreach($messages as $message): ?>
     <div class="message">
-        <?= _("By "); ?><a href="<?= \Controller\Router::url("profile", array("username" => $message['postedBy'])) ?>"><?= $message['postedBy']; ?></a><br />
-        <?= $message['date']; ?><br /><br />
-        <?php if (!empty($message['topic'])){ echo "Topic: " . $message['topic'] . "<br /><br />"; } ?>
-        <?php
-            $message = $message['message'];
-            echo $message; 
-        ?>
+        <div class="by">
+            <?= _("By "); ?><a href="<?= \Controller\Router::url("profile", array("username" => $message['postedBy'])) ?>"><?= $message['postedBy']; ?></a><br />
+        </div>
+        <div class="date">
+            <?= $message['date']; ?>
+        </div>
+        <div class="message-text">
+            <?php if (!empty($message['topic'])){ echo "Topic: " . $message['topic'] . "<br /><br />"; } ?>
+            <?php
+                $message = $message['message'];
+                echo $message; 
+            ?>
+        </div>
     </div>
     <?php endforeach; ?>
