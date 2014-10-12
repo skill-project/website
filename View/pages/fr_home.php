42844ad7a2e12264a25307a42375add004cefb91
Guillaume Sylvestre <gsylvestre@gmail.com>
2014-10-12 07:56:48 +0000

    trad home
    git-svn-id: file:///var/svn/skp/trunk@249 54603a6e-7c00-409b-81b0-d312d09a75cd

--- View/pages/fr_home.php
+++ View/pages/fr_home.php
@@ -1,11 +1,11 @@
 <div id="home-top-panel">
 	<div class="container">
 		<div class="left-content">
-			<h1>Show the world<br />what the world can do.</h1>
-			<p id="tagline">Explore all the human skills. Map them out.</p>
+			<h1>Nous créons la carte des compétences humaines.<br />Et avons besoin de vous.</h1>
+			<p id="tagline">Combien de compétences avez-vous ? Réfléchissez-y, puis cartographiez-les !</p>
 
-			<div id="home-btn-go" class="home-btn"><a href="<?= \Controller\Router::url("graph"); ?>">Start skillwalking</a></div>
-			<div id="home-btn-learn" class="home-btn"><a href="<?= \Controller\Router::url("project"); ?>">Learn more</a></div>
+			<div id="home-btn-go" class="home-btn"><a href="<?= \Controller\Router::url("graph"); ?>">Lancez-vous</a></div>
+			<div id="home-btn-learn" class="home-btn"><a href="<?= \Controller\Router::url("project"); ?>">En savoir plus</a></div>
 		</div>
 
 		<img src="img/SKP-page-home-sun.png" id="home-sun" />
@@ -15,22 +15,23 @@
 <section id="home-main-panel">
 	<div class="container">
 		<img class="home-illus brain-illus" alt="" src="img/SKP-page-home-brain.png" />
-		<h2>Do you know everything you know ?</h2>
-		<p>sda fds fs fs fdfas eq wrew rew rewqr r ewr ew e </p>
+		<h2>Bienvenue sur Skill Project ! Que savez-vous faire ?</h2>
+		<p>Sur Skill Project, nous savons que chacun d'entre nous possède un bagage unique de compétences humaines.<br />Participez à notre communauté et partagez ce que vous savez faire !</p>
 
 		<img class="home-illus network-illus" alt="" src="img/SKP-page-home-network.png" />
-		<h2>Do you know everything you know ?</h2>
-		<p>sda fds fs fs fdfas eq wrew rew rewqr r ewr ew e </p>
+		<h2>Les compétences sont interconnectées</h2>
+		<p>Vous savez programmer ? Alors voilà une compétence !<br />Et "Programmer en C++" ou "Programmer en PHP" sont des sous-compétences de "Prorammer".<br />Et vous connaissez également Wordpress ou Symfony ? Celles-ci sont des sous-compétences de "PHP".
+			<br />Vous comprenez l'idée ?</p>
 
 		<img class="home-illus circle-illus" alt="" src="img/SKP-page-home-circle.png" />
-		<h2>Do you know everything you know ?</h2>
-		<p>sda fds fs fs fdfas eq wrew rew rewqr r ewr ew e </p>
+		<h2>Parlez-en !</h2>
+		<p>En participant à Skill Project, vous contribuez à partageant votre compréhension personnelle de vos compétences.<br />La seule manière d'atteindre notre objectif est de créer une grande communauté diversifiée de personnes prêtes à participer comme vous.<br />Allez-y, parlez-en !</p>
 	</div> 
 </section>
 <section id="home-bottom-panel" class="green">
 	<div class="container">
-		<h2>Ready to go ?</h2>
-		<p>Starts now skillwalking amongst the skill and create your own !</p>
-		<div id="home-btn-go-bottom" class="home-btn"><a href="<?= \Controller\Router::url("graph"); ?>">Start skillwalking</a></div>
+		<h2>Prêt à vous lancer ?</h2>
+		<p>Explorez l'arbre de compétences, et amusez-vous à ajouter les vôtres !</p>
+		<div id="home-btn-go-bottom" class="home-btn"><a href="<?= \Controller\Router::url("graph"); ?>">Commencez !</a></div>
 	</div> 
 </section>
