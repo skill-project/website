a4629c09ddff09041d977a71a30f700d05abbd0d
Dario Spagnolo <i@dariospagnolo.org>
2014-11-12 09:48:30 +0000

    Merge 0.1 into trunk
    git-svn-id: file:///var/svn/skp/trunk@318 54603a6e-7c00-409b-81b0-d312d09a75cd

---
+++ web/js/FPSCounter.js
@@ -0,0 +1,55 @@
+var FPSCounter = function() {
+	this.currentFPS = 0;
+	this.startTime = 0;	
+	this.frames = 0;
+
+	this.running = false;
+	this.averageSent = false;
+
+	this.readings = [];
+};
+
+FPSCounter.prototype.start = function () {
+	if (this.running === true || this.averageSent === true) return;
+	this.running = true;
+	this.startTime = Date.now();
+	
+	requestAnimationFrame($.proxy(this.countFrames, this));
+};
+
+FPSCounter.prototype.end = function () {
+	if (this.running === false || this.averageSent === true) return;
+
+	var timeElapsed = Date.now() - this.startTime;
+	this.currentFPS = Math.round(this.frames / (timeElapsed / 1000));
+
+	this.saveFPS();
+	this.running = false;
+	this.reset();
+};
+
+FPSCounter.prototype.countFrames = function () {
+	this.frames++;
+	if (this.running === true) requestAnimationFrame($.proxy(this.countFrames, this));
+};
+
+FPSCounter.prototype.saveFPS = function() {
+	this.readings.push(this.currentFPS);
+	
+	if (this.readings.length === 5) {
+		var readingsSum = 0;
+		for (readingIndex in this.readings) {
+			readingsSum+=this.readings[readingIndex];
+		}
+		var averageFPS = Math.round((readingsSum / this.readings.length) * 10) / 10;
+
+		ga("send", "event", "averageFPS", "5-readings", averageFPS);
+		this.averageSent = true;
+	}
+}
+
+FPSCounter.prototype.reset = function() {
+	this.currentFPS = 0;
+	this.startTime = 0;
+	this.frames = 0;
+}
