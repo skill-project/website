0327f3b6a35caed8bd71d59f1752e733eaa4d74d
Guillaume Sylvestre <gsylvestre@gmail.com>
2014-12-26 14:27:11 +0000

    factorization
    git-svn-id: file:///var/svn/skp/trunk@400 54603a6e-7c00-409b-81b0-d312d09a75cd

--- Model/NotificationManager.php
+++ Model/NotificationManager.php
@@ -15,7 +15,7 @@
     		parent::__construct();
     	}
 
-        public function sendNotification($type, $params) {
+        public function sendNotification($type, array $params) {
             extract($params);
 
             $skillManager = new SkillManager();
@@ -24,79 +24,79 @@
             switch ($type) {
                 case "add-child":
                     if (!isset($parentSkill) or !isset($newSkill)) throw new \Exception("Required when sending add-child notification : parentSkill, newSkill");
-
-                    //AWAITING SOME FACTORING
                     
                     //Create Notification node
-                    //=======================
-                    $cyp = "CREATE 
-                            (notification:Notification {
-                                uuid: {notificationUuid},
-                                type: {type},
-                                timestamp: {now}
-                            })-[:IS_ABOUT]->(s:Skill {uuid: {newSkillUuid}})";
+                    $notificationUuid = $this->saveNotification($type, $newSkill);
 
-                    $notificationUuid = \Utils\IdGenerator::getUniqueId();
-                        
-                    $namedParams = array(
-                        "now"               => time(),
-                        "notificationUuid"  => $notificationUuid,
-                        "type"              => $type,
-                        "newSkillUuid"      => $newSkill->getUuid()
-                    );
-
-                    $query = new Query($this->client, $cyp, $namedParams);
-                    $resultSet = $query->getResultSet();
-                    //=======================
-
-
-                    
                     //Create relationship with owner
-                    //==============================
-                    $cyp = "MATCH
-                                (u:User {uuid: {ownerUuid} }), 
-                                (n:Notification {uuid: {notificationUuid} }) 
-                            CREATE (u)<-[:SENT {reason: 'owner'}]-(n)";
-
                     $parentSkillOwner = $parentSkill->getOwner();
-
-                    $namedParams = array(
-                        "ownerUuid"             => $parentSkillOwner->getUuid(),
-                        "notificationUuid"  => $notificationUuid
-                    );
-                    $query = new Query($this->client, $cyp, $namedParams);
-                    $resultSet = $query->getResultSet();
-                    //==============================
-
-
+                    $this->saveSingleRelationWithUser($parentSkillOwner, $notificationUuid, "owner");
 
                     //Create as many relationships as there are users who discussed the skill
-                    //=======================================================================
-                    
                     //Retrieves an array of user uuids of users who discussed the skill
                     $usersInDiscussion = $discussionManager->getUsersInDiscussion($parentSkill->getUuid(), false);
-
-                    $cyp = "MATCH
-                                (u:User), 
-                                (n:Notification {uuid: {notificationUuid} }) 
-                            WHERE u.uuid IN ['" . implode("', '", $usersInDiscussion) . "']
-                            CREATE (u)<-[:SENT {reason: 'discussed'}]-(n)";
-
-                    $user = $parentSkill->getOwner();
-
-                    $namedParams = array(
-                        "notificationUuid"  => $notificationUuid
-                    );
-                    $query = new Query($this->client, $cyp, $namedParams);
-                    $resultSet = $query->getResultSet();
-                    //=======================================================================
-
+        
+                    $this->saveMultipleRelationsWithUsers($usersInDiscussion, $notificationUuid, "discussed");
 
                     break;
             }
         }
 
 
-    }
+        protected function saveNotification($type, Skill $skill){
+            
+            $cyp = "MATCH (s:Skill {uuid: {skillUuid}}) 
+                    CREATE 
+                    (notification:Notification {
+                        uuid: {notificationUuid},
+                        type: {type},
+                        timestamp: {now}
+                    })-[:IS_ABOUT]->(s)";
+                
+            $notificationUuid = \Utils\IdGenerator::getUniqueId();
 
-?>
+            $namedParams = array(
+                "now"               => time(),
+                "notificationUuid"  => $notificationUuid,
+                "type"              => $type,
+                "skillUuid"         => $skill->getUuid()
+            );
+
+            $query = new Query($this->client, $cyp, $namedParams);
+            $resultSet = $query->getResultSet();
+
+            return $notificationUuid;
+        }
+
+        protected function saveSingleRelationWithUser(User $user, $notificationUuid, $reason){
+            $cyp = "MATCH
+                        (u:User {uuid: {ownerUuid} }), 
+                        (n:Notification {uuid: {notificationUuid} }) 
+                    CREATE (u)<-[:SENT {reason: {reason}}]-(n)";
+
+            $namedParams = array(
+                "ownerUuid"         => $user->getUuid(),
+                "notificationUuid"  => $notificationUuid,
+                "reason"            => $reason
+            );
+            $query = new Query($this->client, $cyp, $namedParams);
+            $resultSet = $query->getResultSet();
+        }
+
+
+        protected function saveMultipleRelationsWithUsers(array $userUuids, $notificationUuid, $reason){
+            $cyp = "MATCH
+                        (u:User), 
+                        (n:Notification {uuid: {notificationUuid} }) 
+                    WHERE u.uuid IN ['" . implode("', '", $userUuids) . "']
+                    CREATE (u)<-[:SENT {reason: {reason}}]-(n)";
+
+            $namedParams = array(
+                "notificationUuid"  => $notificationUuid,
+                "reason"            => $reason
+            );
+            $query = new Query($this->client, $cyp, $namedParams);
+            $resultSet = $query->getResultSet();
+        }
+
+    }
